a42c79b7e5aaf97c94e019e35d498b6c342dba70
Calvin DiPrete <cpdiprete@gmail.com>
2025-03-02 22:32:49 -0500

    Now able to scan for bluetooth devices on phone

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,3 @@
-// import { Text, View } from "react-native";
-
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <Text>Homepage</Text>
-//     </View>
-//   );
-// }
-
 import { Link } from 'expo-router';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { router } from 'expo-router';
@@ -22,8 +6,8 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
-      <Link href="/bleconnect">View details</Link>
-      <Button title="Go to BLE Connect" onPress={() => router.push('/bleconnect')} />
+      <Link href="/bleconnect">Homepage</Link>
+      <Button title="Connect to Device" onPress={() => router.push('/bleconnect')} />
     </View>
   );
 }
