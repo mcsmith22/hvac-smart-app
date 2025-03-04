f01f6be2fa790ae944346f8cda1410511e570da8
Calvin DiPrete <cpdiprete@gmail.com>
2025-03-03 20:55:19 -0500

    resolved merge conflicts with main

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,4 @@
 import { Stack } from "expo-router";
-
-// <<<<<<< HEAD
-// export default function HomeScreen() {
-// =======
 export default function RootLayout() {
-// >>>>>>> origin/main
   return <Stack />;
 }
